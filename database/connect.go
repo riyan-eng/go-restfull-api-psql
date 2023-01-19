3170da914fee238adeff16b83fb007eb6dd1b45f
riyan-eng <fbriyant11@gmail.com>
2023-01-19 11:56:11 +0700

    connecting to database

---
+++ database/connect.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDb() {
+	var err error
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=%v", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_TIMEZONE"))
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Connection failed to Database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/riyan-eng/go-restfull-api-psql/config"
+	"github.com/riyan-eng/go-restfull-api-psql/database"
+)
+
+func init() {
+	config.LoadEnv()
+	database.ConnectDb()
+}
 
 func main() {
 	// start fiber app
 	app := fiber.New()
+
+	// midleware
+	app.Use(recover.New())
+	app.Use(logger.New())
+	app.Use(cors.New())
 
 	// route
 	app.Get("/", func(c *fiber.Ctx) error {
